406802a0626b383eb578badcc07d6704c5ffdad7
maxiamikel <maxloversist@gmail.com>
2023-12-06 12:50:37 -0300

    Project back-end statment

---
+++ restaurant-back/src/test/java/com/maxi/restaurantback/RestaurantBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.restaurantback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
